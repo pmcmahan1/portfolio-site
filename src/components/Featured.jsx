d397267a62f7e958585cd396c14659edb593de63
unknown <mcmahanconner@gmail.com>
2023-05-12 23:46:41 -0400

    Featured projects added

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const Featured = () => {
   return (
     <div className="featured">
-      <div className="projects-text">Featured Project</div>
+      <div className="featured-item">
+      <div className="featured-main">Featured Project</div>
       <div className="featured-title">Lia's Candles</div>
       <div className="featured-body-text">
         Lia's Candles is an ecommerce site that is only lacking a payment
@@ -30,6 +31,35 @@ const Featured = () => {
         >
           <button className="btn btn-secondary btn-lg">Source Code</button>
         </a>
+        </div>
+      </div>
+      <div className="featured-item">
+      <div className="featured-main">Featured Project</div>
+      <div className="featured-title">Weather App</div>
+      <div className="featured-body-text">
+        One of the first projects I completed without guidance, my weather app was made
+        using React, SASS, and two primary API calls. The first API call is to the google maps API, which
+        allows for my auto-complete search bar and to locate the coords of the place searched.
+        Using these coordinates, I make an API call to the openweathermap API, which gathers all of the data I display including daily and hourly predictions,
+        sunrise and sunset times, etc. In order to translate the data from the weather API to readable
+        information, I use luxon. Best viewed on mobile devices.
+      </div>
+      <div className="featured-buttons">
+        <a
+          href="https://pmcmahan1.github.io/weather-app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="btn btn-primary btn-lg">View Site</button>
+        </a>
+        <a
+          href="https://github.com/pmcmahan1/weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="btn btn-secondary btn-lg">Source Code</button>
+        </a>
+        </div>
       </div>
     </div>
   );
